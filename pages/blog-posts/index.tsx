0e5973af8e20dfbdeedeee4182c286ce927f3ff6
Ahmed Mohamed AbdElMoneam <ahmed.moneam@acutebusiness.com>
2023-06-01 06:49:55 +0300

    initial commit with superplate

---
+++ pages/blog-posts/index.tsx
@@ -0,0 +1,19 @@
+import { AntdListInferencer } from "@refinedev/inferencer/antd";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export default function BlogPostList() {
+  return <AntdListInferencer />;
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
---
+++ pages/categories/create/index.tsx
@@ -0,0 +1,19 @@
+import { AntdCreateInferencer } from "@refinedev/inferencer/antd";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export default function CategoryCreate() {
+  return <AntdCreateInferencer />;
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
